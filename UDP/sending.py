a28f7a57395bd49cc56ba376c1df318a237e59bc
John McCormack <mccormack.wgsi@gmail.com>
2015-05-09 14:11:46 -0400

    increasing the number of packets sent

--- UDP/sending.py
+++ UDP/sending.py
@@ -8,5 +8,7 @@ print "UDP target IP: ", UDP_IP
 print "UDP target Port: ", UDP_PORT
 print "message: ", MESSAGE
 
-sock = socket.socket(socekt.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+for a in range(0, 1000)
+	MESSAGE_SENT = MESSAGE + str(a)
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock.sendto(MESSAGE_SENT, (UDP_IP, UDP_PORT))
