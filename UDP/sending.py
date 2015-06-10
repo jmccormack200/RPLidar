ece656431b3b617a7a96272aefbfa2aa47f72d5d
John McCormack <mccormack.wgsi@gmail.com>
2015-06-10 13:44:24 -0400

    Playing around with the sending file in order to test out the arduino

--- UDP/sending.py
+++ UDP/sending.py
@@ -2,13 +2,14 @@ import socket
 
 UDP_IP = "192.168.0.111"
 UDP_PORT = 2390
-MESSAGE = "1"
+MESSAGE = "10"
 
 print "UDP target IP: ", UDP_IP
 print "UDP target Port: ", UDP_PORT
 print "message: ", MESSAGE
 
-for a in range(0, 1000):
+for a in range(0, 100):
 	MESSAGE_SENT = MESSAGE
+	print a
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	sock.sendto(MESSAGE_SENT, (UDP_IP, UDP_PORT))
